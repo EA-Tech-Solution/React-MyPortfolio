b3fbd37dc0bdb110fee783793e3914257f1c349e
iankde <iankevin.esguerra@gmail.com>
2025-08-01 20:02:54 +0800

    add build script

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/React-MyPortfolio/', // <-- important https://ea-tech-solution.github.io/React-MyPortfolio/
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
   plugins: [react()],
 })
